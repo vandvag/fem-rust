5d00ad7538ad66991da902635b9288777d0775cd
Evangelos Vandoros <vandvag@gmail.com>
2022-02-03 18:25:13 +0200

    changed linalg library to ndarray

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,9 @@
-#[macro_use]
-extern crate rulinalg;
 mod material;
-use rulinalg::matrix::Matrix;
-use rulinalg::vector::Vector;
 
 use material::Material;
 
 fn main() {
-    let a = Matrix::new(3, 3, vec![1, 0, 0, 0, 1, 0, 0, 0, 1]);
-    let b = Vector::new(vec![3, 3, 3]);
-    let mat = Material::new(1000 as f32, 0.3);
+    let mat = Material::new(1000_f32, 0.3);
 
     println!("{}", mat.get_mat_matrix());
     println!("Hello, world!");
--- src/material.rs
+++ src/material.rs
@@ -1,46 +1,38 @@
-use rulinalg::matrix::Matrix;
+use ndarray::array;
+use ndarray::prelude::*;
+use ndarray::Array2;
 
 enum StressCondition {
-	plane_stress,
-	plane_strain,
+    plane_stress,
+    plane_strain,
 }
 
 pub struct Material {
-	mat_matrix: Matrix<f32>,
+    mat_matrix: Array2<f32>,
 }
 
 impl Material {
+    pub fn new(e: f32, nu: f32) -> Material {
+        // Change between plane stress and plain strain stress
+        // TODO: find a way to change between plane stress and plane strain conditions
+        /*
+        let stress_cond = StressCondition::plane_stress;
+        match stress_cond {
+            plane_strain => Material::plane_strain_matrix(e: f32, nu: f32),
+            plane_stress => Material::plane_stress_matrix(e, nu);
+        }
+        */
+        let e1: f32 = e / (1.0 - nu * nu);
+        let e2: f32 = nu * e1;
+        let e3: f32 = e1 * (1.0 - 2.0 * nu) / 2.0;
+        // let mat_matrix = Matrix::new(3, 3, vec![e1, e2, 0.0, e2, e1, 0.0, 0.0, 0.0, e3]);
+        let mat_matrix = array![[e1, e2, 0.0], [e2, e1, 0.0], [0.0, 0.0, e3]];
+        Material {
+            mat_matrix: mat_matrix,
+        }
+    }
 
-	pub fn new(e: f32, nu: f32) -> Material {
-		// Change between plane stress and plain strain stress
-		// TODO: find a way to change between plane stress and plane strain conditions
-		/*
-		let stress_cond = StressCondition::plane_stress;
-		match stress_cond {
-			plane_strain => {
-				let vor: f32 = E / ((1.0 + nu) * (1.0 - 2.0 * nu));
-				let e1: f32 = vor * (1.0 - nu);
-				let e2: f32 = vor * nu;
-				let e3: f32 = vor * (1.0 - 2.0 * nu) / 2.0;
-			}
-			plane_stress => {
-				let e1: f32 = E / (1.0 - nu * nu);
-				let e2: f32 = nu * e1;
-				let e3: f32 = e1 * (1.0 - 2.0 * nu) / 2.0;
-			}
-		}
-		*/
-		// plane stress settings
-		let e1: f32 = e / (1.0 - nu * nu);
-		let e2: f32 = nu * e1;
-		let e3: f32 = e1 * (1.0 - 2.0 * nu) / 2.0;
-		let mat_matrix = Matrix::new(3, 3, vec![e1, e2, 0.0, e2, e1, 0.0, 0.0, 0.0, e3]);
-		Material {
-			mat_matrix: mat_matrix,
-		}
-	}
-
-	pub fn get_mat_matrix(&self) -> &Matrix<f32> {
-		&self.mat_matrix
-	}
+    pub fn get_mat_matrix(&self) -> &Array2<f32> {
+        &self.mat_matrix
+    }
 }
