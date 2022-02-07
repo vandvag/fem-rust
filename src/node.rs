002f0492f106767c9286f7d753519f15dfe50dc3
Evangelos Vandoros <vandvag@gmail.com>
2022-02-07 16:40:06 +0200

    added todo comments

--- src/node.rs
+++ src/node.rs
@@ -1,14 +1,14 @@
 #[derive(Debug)]
 pub struct Node {
     id: i32,
-    coords: Vec<f64>
+    coords: Vec<f64>,
 }
 
 impl Node {
-	pub fn new(_id: i32, _x: f64, _y: f64) -> Node {
-		Node {
-			id: _id,
-			coords: Vec::from([_x, _y])
-		}
-	}
+    pub fn new(_id: i32, _x: f64, _y: f64) -> Node {
+        Node {
+            id: _id,
+            coords: Vec::from([_x, _y]),
+        }
+    }
 }
